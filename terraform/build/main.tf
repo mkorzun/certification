09073b9a3e7702eae543bc257c6769c75d36195e
Mayk <kmi8652@gmail.com>
2022-03-19 20:21:50 +0300

    creating a workspace

---
+++ terraform/build/main.tf
@@ -0,0 +1,68 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state.snuffles765"
+    key = "workspaces/build/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-up-and-runninglocks"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "subnet_id" {
+  default = "subnet-0ee089a33dba6fbc7"
+}
+
+variable "vpc_id" {
+  default = "vpc-0b2ea4ec8edac3524"
+}
+
+variable "image_id" {
+  default = "ami-04505e74c0741db8d"
+}
+
+resource "aws_s3_bucket" "bk" {
+   bucket = "boxfuse.snuffles999"
+}
+
+resource "tls_private_key" "key" {
+ algorithm = "RSA"
+ rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "aws_key" {
+ key_name   = "aws-ssh-key"
+ public_key = tls_private_key.key.public_key_openssh
+}
+
+resource "aws_security_group" "build_group" {
+  name        = "build_group"
+  vpc_id      = "${var.vpc_id}"
+
+
+  ingress {
+    description = "ssh access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "build_instance" {
+  ami = "${var.image_id}"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.aws_key.key_name
+  vpc_security_group_ids = ["${aws_security_group.build_group.id}"]
+  subnet_id = "${var.subnet_id}"
+}
--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state.snuffles765"
+    key = "workspaces/stage/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-up-and-runninglocks"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -28,29 +38,8 @@ resource "aws_key_pair" "aws_key" {
  public_key = tls_private_key.key.public_key_openssh
 }
 
-resource "aws_security_group" "builder_group" {
-  name        = "builder_group"
-  vpc_id      = "${var.vpc_id}"
-
-
-  ingress {
-    description = "ssh access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "production_group" {
-  name        = "production_group"
+resource "aws_security_group" "stage_group" {
+  name        = "stage_group"
   vpc_id      = "${var.vpc_id}"
 
   ingress {
@@ -77,18 +66,10 @@ resource "aws_security_group" "production_group" {
   }
 }
 
-resource "aws_instance" "builder_instance" {
+resource "aws_instance" "stage_instance" {
   ami = "${var.image_id}"
   instance_type = "t2.micro"
   key_name = aws_key_pair.aws_key.key_name
-  vpc_security_group_ids = ["${aws_security_group.builder_group.id}"]
-  subnet_id = "${var.subnet_id}"
-}
-
-resource "aws_instance" "production_instance" {
-  ami = "${var.image_id}"
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.aws_key.key_name
-  vpc_security_group_ids = ["${aws_security_group.production_group.id}"]
+  vpc_security_group_ids = ["${aws_security_group.stage_group.id}"]
   subnet_id = "${var.subnet_id}"
 }
