e5399e8d60f79913013ee8f6da74ed80e0ee049d
Mayk <kmi8652@gmail.com>
2022-03-19 23:49:07 +0300

    fix

--- terraform/build/main.tf
+++ terraform/build/main.tf
@@ -64,7 +64,7 @@ resource "aws_instance" "build_instance" {
   instance_type = "${var.instance_type}"
   key_name = aws_key_pair.build.key_name
   vpc_security_group_ids = ["${aws_security_group.build_group.id}"]
-  subnet_id = "${var.subnet_id}"
+#  subnet_id = "${var.subnet_id}"
   tags = {
     Name = "build"
   }
